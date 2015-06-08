45d1d580b2f3675510aa3c3e57faf9bcd65d4388
Nick Ong <ongkh71@NOMACPRO.local>
2015-06-09 00:21:45 +0800

    added nameLabel for challenge 1

--- PostCard/ViewController.swift
+++ PostCard/ViewController.swift
@@ -13,10 +13,11 @@ class ViewController: UIViewController {
     
     
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
-  
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,10 +37,16 @@ class ViewController: UIViewController {
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         
+        // For the nameLabel
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        enterNameTextField.text = ""
         enterMessageTextField.text = ""
         //To move the keyboard away
         enterMessageTextField.resignFirstResponder()
-        
+
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
         
         
